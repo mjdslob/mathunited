a619d6b00a2b1c9ce6a58ebae70a2ec4baf4f31e
Martijn Slob <mjdslob@gmail.com>
2014-04-04 07:33:35 +0200

    editor: working on AlgebraKIT integration

---
+++ Author-java/MathUnited/src/main/webapp/javascript/editor/algebrakit/Widget.js
@@ -0,0 +1,54 @@
+/* 
+ * Copyright (C) 2013 Martijn Slob <m.slob@math4all.nl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+define(['algebrakit/Engine', 'jquery','jqueryui','jqueryChosen'], function(engine, $) {
+
+    var dialog_html = 
+            '<div class="akit-container">'
+           +'  <select class="audience-choser" data-placeholder="selecteer de doelgroep...">'
+           +'     <option value="rekenen">rekenen</option>'
+           +'     <option value="onderbouw">onderbouw</option>'
+           +'     <option value="vwo-b">bovenbouw</option>'
+           +'  </select>'
+           +'  <div class="akit-input-wrapper">'
+           +'     Invoer: <input type="text" width=80></input>'
+           +'  </div>'
+           +'  <div class="akit-derivation"></div>'
+           +'  <div class="akit-button-container"><div class="akit-button akit-cancel">annuleren</div><div class="akit-button akit-ok">OK</div></div>'
+           +'</div>'
+
+    return {
+        show: function(parent, callback_ok) {
+            var dialog = $(dialog_html).dialog({width:300, height:400});
+            $('.audience-choser', dialog).chosen({width:200});
+            $('.akit-ok',dialog).click(function() {
+                    //callback_ok();
+                    dialog.dialog('close');
+            });
+            $('.akit-cancel',dialog).click(function() {
+                    dialog.dialog('close');
+            });
+            $('.akit-input-wrapper input').change(function() {
+               var exp=$(this).val(); 
+               debugger;
+               engine.solve(exp, 'vwo-b', function(data) {
+                   debugger;
+               });
+            });
+        }  
+    };
+});
