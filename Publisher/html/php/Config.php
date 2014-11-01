b7571ef69f6c2844d5873490f9831439e4901baf
Martijn Slob <mjdslob@gmail.com>
2014-11-01 23:13:19 +0100

    toevoegen vwo-c

--- Publisher/html/php/Config.php
+++ Publisher/html/php/Config.php
@@ -6,7 +6,7 @@ class Config {
         'm4a' => array(
             'id'=>'m4a',
             'paths'=>array(
-                'vd/', 'vb/','va/','v3/',
+                'vd/', 'vc/', 'vb/','va/','v3/',
                 'hv/',
                 'h3/',
                 'ha/',
