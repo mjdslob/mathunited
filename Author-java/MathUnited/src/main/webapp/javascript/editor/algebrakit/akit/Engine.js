d8a5996f94f32fddcd0f01208f676eb72b7f0865
Martijn Slob <mjdslob@gmail.com>
2015-09-25 09:47:49 +0200

    JIRA PXKETEN-267: fixed bug in AlgebraKIT test-widget

--- Author-java/MathUnited/src/main/webapp/javascript/editor/algebrakit/akit/Engine.js
+++ Author-java/MathUnited/src/main/webapp/javascript/editor/algebrakit/akit/Engine.js
@@ -7,8 +7,8 @@
 define(['jquery','akit/WKStep'], function ($, WKStep) {
 //    var engineUrl = 'http://mathunited.pragma-ade.nl:41080/AKIT_RemoteServer/Main';
 //    var engineUrl = 'http://localhost/AKITServer/Main';
-//    var engineUrl = '/AKITServer/Main';
-    var engineUrl = 'http://akit-server-2014.appspot.com/Main';
+    var engineUrl = '/AKITServer/Main';
+//    var engineUrl = 'http://akit-server-2014.appspot.com/Main';
     var engineVersionUrl = 'http://{VERSION}.akit-server-2014.appspot.com/Main';
     function variablesToString(varr) {
         if (!varr)
@@ -146,7 +146,7 @@ define(['jquery','akit/WKStep'], function ($, WKStep) {
             var params = 'cmd=getsolutionmodel_fromexpr&audience=' + spec.audience
                     + '&expression=' + encodeURIComponent(spec.expression);
             if (spec.answer)
-                params += '&answer=' + encodeURIComponent(JSON.stringify(spec.answer));
+                params += '&answer=' + encodeURIComponent(spec.answer);
             if (spec.mode)
                 params += '&mode=' + spec.mode;
             if (spec.attributes)
