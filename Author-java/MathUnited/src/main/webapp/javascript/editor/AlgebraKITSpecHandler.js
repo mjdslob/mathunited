2af80c174be917865582b800c7388dda1ef91582
Martijn Slob <mjdslob@gmail.com>
2015-11-30 12:06:00 +0100

    verwijder poort :8080 uit URL naar AlgebraKIT

--- Author-java/MathUnited/src/main/webapp/javascript/editor/AlgebraKITSpecHandler.js
+++ Author-java/MathUnited/src/main/webapp/javascript/editor/AlgebraKITSpecHandler.js
@@ -85,7 +85,7 @@ define(['exercise/Main','trainer/Main','jquery'], function(AKITExercise, AKITTra
                 chooseSelectedItem('algebrakit-hint-select', tag, 'show-hints', 'true', true);
                 
 //                var algebrakitURL = 'http://localhost:8080/akit';
-                var algebrakitURL = 'http://217.23.8.140:8080/akit-staging';
+                var algebrakitURL = 'http://217.23.8.140/akit-staging';
                 
                 $('.algebrakit-review-process',parent).first().unbind('click').click(function() {
                     var audienceStr = $('select.audience-select option:selected', parent).attr('value');
