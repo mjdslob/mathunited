f08c76393e793ab9cdc53f97c0699ba30acf580c
Bas van der Linden <bas.linden@gmail.com>
2016-12-22 22:32:11 +0100

    Added explicit rereading of workflow map.

--- Author-java/MathUnited/src/main/java/nl/math4all/mathunited/editor/WorkflowServlet.java
+++ Author-java/MathUnited/src/main/java/nl/math4all/mathunited/editor/WorkflowServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import nl.math4all.mathunited.configuration.Component;
 import nl.math4all.mathunited.configuration.SubComponent;
 import nl.math4all.mathunited.configuration.Configuration;
@@ -21,34 +22,38 @@ import nl.math4all.mathunited.configuration.Repository;
 import nl.math4all.mathunited.utils.Utils;
 
 /**
- *
  * @author Martijn Slob <m.slob@math4all.nl>
  */
 public class WorkflowServlet extends HttpServlet {
     private final static Logger LOGGER = Logger.getLogger(WorkflowServlet.class.getName());
 
-    /** map of repo to a (map of subcomp-id to status) */
-    private static Map<String, Map<String, String>> statusMap=new HashMap<String, Map<String, String>>();
-    
+    /**
+     * map of repo to a (map of subcomp-id to status)
+     */
+    private static Map<String, Map<String, String>> statusMap = new HashMap<String, Map<String, String>>();
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         LOGGER.setLevel(Level.INFO);
     }
 
-    public static void clear() { statusMap.clear(); }
+    public static void clear() {
+        statusMap.clear();
+    }
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try{
+        try {
             LOGGER.fine("Request: WorkflowServlet");
             response.setContentType("application/xml");
             Writer w = response.getWriter();
@@ -56,74 +61,75 @@ public class WorkflowServlet extends HttpServlet {
             Map<String, String> parameterMap = Utils.readParameters(request);
 
             String repo = parameterMap.get("repo");
-            
+            String reread = parameterMap.get("reread");
+
             Map<String, String> repoMap = statusMap.get(repo);
-            if (repoMap == null) {
+            if (repoMap == null || reread == "true") {
                 readComponentStatus(repo);
                 repoMap = statusMap.get(repo);
-                if(repoMap == null) {
-                    throw new Exception("<error>Could not get info on repository "+repo+"</error");
+                if (repoMap == null) {
+                    throw new Exception("<error>Could not get info on repository " + repo + "</error");
                 }
             }
-            
-            pw.println("<repo id='"+repo+"'>");
-            for(Map.Entry<String, String> entry : repoMap.entrySet()) {
-                pw.println("<subcomp id='"+entry.getKey()+"' status='"+entry.getValue()+"'/>");
+
+            pw.println("<repo id='" + repo + "'>");
+            for (Map.Entry<String, String> entry : repoMap.entrySet()) {
+                pw.println("<subcomp id='" + entry.getKey() + "' status='" + entry.getValue() + "'/>");
             }
             pw.println("</repo>");
-            
-        }
-        catch (Exception e) {
+
+        } catch (Exception e) {
             Utils.writeError(response, e);
         }
-        
+
     }
 
     public static void updateStatus(String repo, String subcomp, String path) throws Exception {
         Map<String, String> compStatusMap = statusMap.get(repo);
-        if(compStatusMap!=null) {
+        if (compStatusMap != null) {
             String status = extractStatus(path);
             compStatusMap.put(subcomp, status);
         }
     }
-    
+
     private void readComponentStatus(String repo) throws Exception {
         Configuration config = Configuration.getInstance();
 
         Map<String, Repository> repoMap = config.getRepos();
         Repository repository = repoMap.get(repo);
-        if(repository==null) {
-            throw new Exception("Onbekende repository: "+repo);
+        if (repository == null) {
+            throw new Exception("Onbekende repository: " + repo);
         }
 
         Map<String, Component> componentMap = repository.readComponentMap();
 
         Map<String, String> compStatusMap = statusMap.get(repo);
-        if(compStatusMap==null) {
+        if (compStatusMap == null) {
             compStatusMap = new HashMap<String, String>();
             statusMap.put(repo, compStatusMap);
         }
-        for(Component cc : componentMap.values()) {
-            for(SubComponent ss : cc.subComponentList) {
+        for (Component cc : componentMap.values()) {
+            for (SubComponent ss : cc.subComponentList) {
                 String status = readSubcomponentStatus(ss, repository, config);
                 compStatusMap.put(ss.id, status);
             }
         }
     }
 
-    private String readSubcomponentStatus(SubComponent sub,  Repository repository, Configuration config) throws IOException{
+    private String readSubcomponentStatus(SubComponent sub, Repository repository, Configuration config) throws IOException {
         String result = "unknown";
-        String path = config.contentRoot+repository.getPath()+"/"+sub.file;
+        String path = config.contentRoot + repository.getPath() + "/" + sub.file;
 
         return extractStatus(path);
     }
-    
-    private static String extractStatus(String path)  throws IOException{
+
+    private static String extractStatus(String path) throws IOException {
         String result = null;
+
         //don't parse the whole document, just search the for the text: <subcomponent ... status=".." ..>
         //note that the status can be absent.
 
-        BufferedReader br=null;
+        BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(path));
             StringBuilder sb = new StringBuilder();
@@ -135,37 +141,43 @@ public class WorkflowServlet extends HttpServlet {
                 line = br.readLine();
             }
             String text = sb.toString();
-            
+
             int i0 = text.indexOf("<subcomponent");
-            if(i0<0) throw new Exception("Invalid subcomponent: "+path);
-            int i1 = text.indexOf(">",i0);
-            if(i1<0) throw new Exception("Invalid subcomponent: "+path);
-            int ii = text.indexOf("status",i0);
-            if(ii>0) {
-                i0 = text.indexOf("\"",ii);
-                i1 = text.indexOf("\"",i0+1);
-                if(i0>0 && i1>i0) {
-                    result = text.substring(i0+1, i1);
-                }
-                LOGGER.log(Level.FINE, "Found status: subcomp={0}, status={1}", new Object[]{path, result,i0,i1});
+            if (i0 < 0) {
+                throw new Exception("Invalid subcomponent: " + path);
             }
-            
-        } catch(Exception e) {
-            LOGGER.severe("Error occurred: "+e.getMessage());
+
+            int i1 = text.indexOf(">", i0);
+            if (i1 < 0) {
+                throw new Exception("Invalid subcomponent: " + path);
+            }
+            int ii = text.indexOf("status", i0);
+            if (ii > 0) {
+                i0 = text.indexOf("\"", ii);
+                i1 = text.indexOf("\"", i0 + 1);
+                if (i0 > 0 && i1 > i0) {
+                    result = text.substring(i0 + 1, i1);
+                }
+                LOGGER.log(Level.FINE, "Found status: subcomp={0}, status={1}", new Object[]{path, result, i0, i1});
+            }
+
+        } catch (Exception e) {
+            LOGGER.severe("Error occurred: " + e.getMessage());
         } finally {
-            if(br!=null) br.close();
+            if (br != null) br.close();
         }
-        if(result==null) result="unknown";
+        if (result == null) result = "unknown";
         return result;
     }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -176,10 +188,10 @@ public class WorkflowServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
